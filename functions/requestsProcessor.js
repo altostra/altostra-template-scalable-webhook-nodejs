15e15b28e9302843eef8e14f695dc4aae6d1116e
Yossale <yossi@altostra.com>
2020-07-27 19:13:16 +0300

    update

--- functions/requestsProcessor.js
+++ functions/requestsProcessor.js
@@ -1,5 +1,32 @@
 const util = require('util')
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const REQ_TABLE = process.env.TABLE_REQUESTSTABLE01
+
+async function writeMessageToTable(msg) {
+
+  const params = {
+    TableName: REQ_TABLE,
+    Item: {
+      'id': msg.emailId,
+      'status': msg.eventType,
+      'updated_at': `${new Date().toISOString()}`
+    }
+  };
+
+  try {
+    const data = await docClient.put(params).promise();
+    console.log("Added item:", JSON.stringify(data, null, 2));
+  } catch (err) {
+    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+  }
+
+}
 
 module.exports.handler = async (event, context) => {
   console.log(`Received SQS event: ${util.inspect(event)}`)
+
+  const msg = JSON.parse(event.Records[0].body)
+  await writeMessageToTable(msg)
 }
--- functions/requestsVerifier.js
+++ functions/requestsVerifier.js
@@ -1,5 +1,5 @@
-import { SQS } from 'aws-sdk';
-const sqs = new SQS({ apiVersion: '2012-11-05' });
+const AWS = require('aws-sdk')
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 
 const queueUrl = process.env.QUEUE_REQUESTSQUEUE01
 
@@ -7,18 +7,23 @@ exports.handler = async (event) => {
 
   console.info(`Received event:  ${JSON.stringify(event)}`)
 
+  const msgBody = event.body
+
   var params = {
-    MessageBody: "This is a test message.",
-    QueueUrl: sqsUrl
+    MessageBody: msgBody,
+    QueueUrl: queueUrl
   };
 
-  const res = await sqs.sendMessage(params).promise()
+  try {
+    const res = await sqs.sendMessage(params).promise()
+    console.info(`Queue message sent: ${JSON.stringify(res)}`)
 
-  console.info(`Queue message sent: ${JSON.stringify(res)}`)
+  } catch (e) {
+    console.error(`Failed to send message to queue`, e)
+  }
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+  return {
+    statusCode: 200
+  }
+
 };
